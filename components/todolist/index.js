f90ea7ee9b86911658d8c6540b4723f3c560d539
Austin Pickett <pickett.austin@gmail.com>
2018-07-13 06:50:31 -0500

    remove item.js - make functional component

--- components/todolist/index.js
+++ components/todolist/index.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react'
-import Item from './item'
+import styled from 'styled-components'
+import { withStateHandlers } from 'recompose';
+
+const ItemRow = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  font-family: Arial;
+
+  &.complete {
+    color: grey;
+    text-decoration: line-through;
+  }
+`
+
+const Item = ({title, complete}) => (
+  <ItemRow>
+    <input
+      type="checkbox"
+      checked={complete}
+      onClick={el => el.target.offsetParent.classList.toggle('complete')}
+    />
+    <strong>{title}</strong>
+  </ItemRow>
+)
 
 export default class extends Component {
 	constructor(props) {
@@ -7,9 +30,9 @@ export default class extends Component {
 
 		this.state = {
 			todo_list: [
-				{ title: 'lorem ipsum dolor sit amit', complete: false },
-				{ title: 'Do Laundry', complete: false },
-				{ title: 'Do Dishes', complete: false }
+				{ title: 'lorem ipsum dolor sit amit', complete: false, id: 1 },
+				{ title: 'Do Laundry', complete: false, id: 2 },
+				{ title: 'Do Dishes', complete: false, id: 3 }
 			]
     }
 
@@ -17,27 +40,26 @@ export default class extends Component {
   }
 
   handleClick() {
-    const newItem = {
-      title: document.getElementById('todo-title').value,
-      complete: false
-    }
-
     const todo_list = this.state.todo_list
-    todo_list.push(newItem)
+    todo_list.push({
+      title: document.getElementById('todo-title').value,
+      complete: false,
+      id: todo_list[todo_list.length - 1] + 1
+    })
 
     this.setState({ todo_list })
   }
 
 	render() {
-    console.log(this.state.todo_list)
-
 		return (
 			<div>
 				<input type="text" placeholder="Add item" id="todo-title"/>
 				<input type="submit" onClick={this.handleClick} />
 
-				<div className="list">{this.state.todo_list.map(x => <Item title={x.title} complete={x.complete} key={Math.random()}/>)}</div>
+        <div className="list">
+          {this.state.todo_list.map(x => <Item title={x.title} complete={x.complete} key={x.id} />)}
+        </div>
 			</div>
-		);
+		)
 	}
 }
